2daf49d9a97f58ad83de5823caf02a80d6fbf90c
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-04-20 00:16:31 +0900

    Add: CookieParser, morgan, static middleware - CookieParser는 쿠키 생성 및 삭제를 도와주는 모듈(위조 방지를 위한 사인   옵션등이 중요함) - morgan 패키지는 요청과 응답에 대한 정보를 콘솔에 기록함 dev, combined   옵션 등 존재 - static은 기본제공 미들웨어로 정적파일들 제공하는 라우터 역할을 한다.   __dirname활용 가능

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,9 +1,19 @@
 //express모듈을 실행해 app변수에 할당한다.
 //express 내부에 Http모듈이 내장되어 있으므로 서버의 역할을 할 수 있다.
 const express = require("express");
+
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+//dotenv 패키지는 .env 파일을 읽어서 process.env로 변경한다.
+const dotenv = require("dotenv");
 //파일 경로 지정을 위해 path모듈 사용
 const path = require("path");
+//body-parser는 요청의 본문에 있는 데이터를 해석해서 req.body객체로 만들어 주는 미들웨어이고 보통 폼데이터나 AJAX요청의 데이터를 처리한다.
+//단 멀티파트(이미지, 동영상, 파일 등)의 데이터는 처리하지 못하며 이는 multuer모듈을 사용하면 된다.
+const bodyParser = require("body-parser");
 
+dotenv.config();
 const app = express();
 //app.set("port", 포트)로 서버가 실행될 포트를 설정한다.
 //process.env객체에 PORT 속성이 있다면 그 값을 사용하고, 없다면 기본 값으로 3000번 포트를 이용하도록 되어있다!
@@ -11,9 +21,80 @@ const app = express();
 //이 데이터를 나중에 app.get(키)로 가져올 수 있다.
 app.set("port", process.env.PORT || 3000);
 
+//설치했던 패키지들을 아래에 연결한다
+//app.use에 연결할 때 req,res,next 같은 것들이 보이지 않지만 미들웨어 내부에 들어있는 것이다.
+//next도 내부적으로 호출하기에 다음 미들웨어로 넘어갈 수 있다.
+
+//morgan 패키지는 요청과 응답에 대한 정보를 콘솔에 기록한다.
+app.use(morgan("dev"));
+//... GET / 500 9.739 ms - 35 는 morgan 미들웨어에서 나오는 것이다.
+//옵션 중 dev와 combinde, short등이 있는데 보통은 개발에서 dev를 , 배포환경에서는 combined를 사용한다고 한다.
+//[HTTP 메서드][주소][HTTP상태코드][응답속도]-[응답바이트]의 형태이다
+//요청과 응답에 대한 정보를 콘솔에 기록하게 된다.
+
+//static 미들웨어는 정적인 파일들을 제공하는 라우터 역할을 한다. 기본제공이기에 따로 설치필요 없이 express객체 안에서 꺼내 장착하면 된다.
+//app.use(요청경로, express.static("실제경로"))
+//예를들어 public/stylesheet/style.css -> https://localhost:3000/stylesheet/style.css로 접근할 수 있다.
+//public폴더를 만들고 css,js,img를 넣으면 브라우저에서 접근이 가능해진다
+//만약 파일이 없으면 알아서 내부적으로 next를 호출하고 파일을 발견했다면 next를 호출하지 않고 파일을 보내게 되므로 다음 미들웨어는 실행되지 않는다
+app.use("/", express.static(path.join(__dirname, "public")));
+
+//body-parser는 요청의 본문에 있는 데이터를 해석해서 req.body객체로 만들어 주는 미들웨어이고 보통 폼데이터나 AJAX요청의 데이터를 처리한다.
+//단 멀티파트(이미지, 동영상, 파일 등)의 데이터는 처리하지 못하며 이는 multuer모듈을 사용하면 된다.
+//기본적으로 4.16.0 버전부터 json, urlencoded는 내장되어 있고 text,raw데이터를 추가로 해석하기 위해서는 설치를 해야한다
+//Raw는 요청의 본문이 버퍼 데이터 일때, Text는 텍스트 데이터일 때 해석하는 미들웨어이다.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
+
+//process.env.COOKIE_SECRET에 cookiesecret 값이 할당된다.
+//키 = 값 형식으로 추가하면 된다. process.env를 별도의 파일로 관리하는 이유는 보안과 설정의 편의성 때문이다.
+//cookieParser는 요청에 동봉된 쿠키를 해석해 req.cookie 객체로 만든다. 4.3절의 parseCookie 함수와 기능이 비슷하다.
+//  //{ 'ch-veil-id': 'b1f5def4-1d8f-4701-b173-a00f576ecde2' } 이렇게 req.cookies를 찍으면 나오는 것을 볼 수 있다.
+
+app.use(cookieParser({ name: "sanhunlee" }));
+
+//서명된 쿠키가 있는 경우 내 서버가 만든 쿠키임을 검증할 수 있다.
+//쿠키는 클라이언트에서 위조하기 쉬우므로 비밀키를 통해 만들어낸 서명을 쿠키값 뒤에 붙인다
+//서명이 붙으면 name=sanghun.sign과 같은 모양이 된다.
+//서명이 된 쿠키는 req.cookies대신 req.signedCookies에 있다.
+// app.use((req, res, next) => {
+//   //쿠키생성을 위해서는 res.cookie()
+//   res.cookie("name", "sanghunLee", {
+//     expires: new Date(Date.now() + 900000),
+//     httpOnly: true,
+//     secure: true,
+//   });
+
+//   //쿠키삭제를 위해서는 res.cleaerCooke()
+//   //쿠키를 지우기 위해서는 키와 값 외에도 옵션값이 정확히 일치해야한다.
+//   //signed라는 옵션이 존재하는데 이를 true로 설정하면 쿠키 뒤에 서명이 들어가므로 대부분의 경우 서명옵션을 켜두는 것이 좋다.
+//   //서명을 위한 비밀 키는 cookieParser 미들웨어에 인수로 넣은 process.env.COOKIE_SECRET이 된다.
+//   res.clearCookie("name", "sanghunLee", {
+//     httpOnly: true,
+//     secure: true,
+//   });
+// });
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
 //주소를 첫번째 인수로 넣어주지 않으면 미들웨어는 모든요청에서 실행되고, 주소를 넣는다면 해당하는 요청에서만 실행된다고 보면된다.
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됩니다.");
+  console.log("@@@");
+  console.log(req.cookies);
+  console.log("@@@@");
   next();
 });
 
